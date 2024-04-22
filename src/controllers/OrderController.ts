0d14d811d9ed9184af0a8c0615a16d0f4443e0be
sonlh02 <let02hb@gmail.com>
2024-04-22 11:05:20 +0700

    Added Create session API

---
+++ src/controllers/OrderController.ts
@@ -0,0 +1,128 @@
+import { Request, Response } from "express";
+import Stripe from "stripe";
+import Restaurant, { MenuItemType } from "../models/restaurant";
+
+const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
+
+const FRONTEND_URL = process.env.FRONTEND_URL as string;
+
+type CheckoutSessionRequest = {
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    email: string;
+    name: string;
+    addressLine1: string;
+    district: string;
+  };
+  restaurantId: string;
+};
+
+const createCheckoutSession = async (req: Request, res: Response) => {
+  try {
+    const checkoutSessionRequest: CheckoutSessionRequest = req.body;
+
+    const restaurant = await Restaurant.findById(
+      checkoutSessionRequest.restaurantId
+    );
+
+    if (!restaurant) {
+      throw new Error("Restaurant not found");
+    }
+
+    const lineItems = createLineItems(
+      checkoutSessionRequest,
+      restaurant.menuItems
+    );
+
+    const session = await createSession(
+      lineItems,
+      "TEST_ORDER_ID",
+      restaurant.deliveryPrice,
+      restaurant._id.toString()
+    );
+
+    if (!session.url) {
+      return res.status(500).json({ message: "Error creating stripe session" });
+    }
+
+    res.json({ url: session.url });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({ message: error.raw.message });
+  }
+};
+
+const createLineItems = (
+  checkoutSessionRequest: CheckoutSessionRequest,
+  menuItems: MenuItemType[]
+) => {
+  //1. foreach cartItem - lặp qua cartItem, lấy đối tượng menuItem từ nhà hàng => để có được giá
+  //2. foreach cartItem - lặp qua cartItem, chuyển đổi nó thành một stripe line item
+  //3. return line item array
+
+  const lineItems = checkoutSessionRequest.cartItems.map((cartItem) => {
+    const menuItem = menuItems.find(
+      (item) => item._id.toString() === cartItem.menuItemId.toString()
+    );
+
+    if (!menuItem) {
+      throw new Error(`Menu item not found: ${cartItem.menuItemId}`);
+    }
+
+    const line_item: Stripe.Checkout.SessionCreateParams.LineItem = {
+      price_data: {
+        currency: "gbp",
+        unit_amount: menuItem.price,
+        product_data: {
+          name: menuItem.name,
+          //   description: menuItem.description,
+          //   images: menuItem.images,
+        },
+      },
+      quantity: parseInt(cartItem.quantity),
+    };
+    return line_item;
+  });
+
+  return lineItems;
+};
+
+const createSession = async (
+  lineItems: Stripe.Checkout.SessionCreateParams.LineItem[],
+  orderId: string,
+  deliveryPrice: number,
+  restaurantId: string
+) => {
+  const sessionData = await STRIPE.checkout.sessions.create({
+    line_items: lineItems,
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          display_name: "Vận chuyển",
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: deliveryPrice,
+            currency: "gbp",
+          },
+        },
+      },
+    ],
+    mode: "payment",
+    metadata: {
+      orderId,
+      restaurantId,
+    },
+    success_url: `${FRONTEND_URL}/order-status?success=true`,
+    cancel_url: `${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
+  });
+
+  return sessionData;
+};
+
+export default {
+  createCheckoutSession,
+}
